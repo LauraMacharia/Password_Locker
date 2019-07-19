29687a2636d5db6fe924049520307e94a67b92f9
Laura Macharia <lauramacharia117@gmail.com>
2019-07-19 14:27:50 +0300

    created the folder user.py

---
+++ user.py
@@ -0,0 +1,14 @@
+# class User:
+#     """
+#     Class generates new instances of the user
+#     """
+    
+#     user_list = []
+#     def __init__(self, first_name, last_name, phone_number, email, user_name, pasword):
+        
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.phone_number = phone_number
+#         self.email = email
+#         self.user_name = user_name
+#         self.pasword = pasword
