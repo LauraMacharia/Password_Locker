7698466a554e2fa85170efa8d14e7e016345eb5c
Laura Macharia <lauramacharia117@gmail.com>
2019-07-22 07:52:04 +0300

    changes

--- user.py
+++ user.py
@@ -1,24 +1,61 @@
 class User:
     """
-    Class generates new instances of the user
+    Class that generates new instances of user
     """
     
-    user_list = []
-    def __init__(self, first_name, last_name, phone_number, email, user_name, pasword):
-        
+
+    user = []
+
+    def __init__(self,first_name,last_name,phone_number,email,username,password):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
-        self.user_name = user_name
-        self.pasword = pasword
-        
-        
+        self.username = username
+        self.password = password
+
     def save_user(self):
-        """
-        save_user saves contact objects to the user
-        """
+        '''
+        save_user method saves contact objects into user
+        '''
+        User.user.append(self)
+
+    def delete_user(self):
+        '''
+        delete_user method deletes a saved user from the user
+        '''
+        User.user.remove(self)
         
-        User.user_list.append(self)    
-        
-      
+    @classmethod
+    def find_by_number(cls,number):
+        '''
+        method that takes in a number and returns a user that matches that number
+        Args:
+            number:phone number to search
+        Returns:
+            User of that matches te number.
+        '''
+        for user in cls.user:
+            if user.phone_number == number:
+                return user
+
+    @classmethod
+    def user_exist(cls,username):
+        '''
+        Method that checks if a user exists from the user list.
+        Args:
+            number:phone number to search if it exists
+        Returns:
+            Boolean:True or false depending if the user exists
+        '''
+        for user in cls.user:
+            if user.username == username:
+                return True
+        return False
+
+    @classmethod
+    def display_user(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user
--- user_test.py
+++ user_test.py
@@ -1,38 +1,91 @@
 import unittest
 from user import User
 
-class Testuser(unittest.TestCase):
-    """
-    test clas define test cases for the user class behaviours.
-    
-    Args
-    unittest.TestCase: TestCase class that help in creating test cases
-    """
-    def setup(self):
-        
-        self.new_user = User("Laura", "Macharia", "0712036591", "lauramacharia@gmail.com", "LauraMacharia", "ruvy")
-        
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviour.
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Laura","Macharia","0712036591","lauramacharia@gmail.com","LauraMacharia","ruvy") 
+
+
     def test_init(self):
-        """
-        To test if the objects are initialized properly
-        """
-        
-        self.assertEqual(self.new_user.first_name, "Laura")
-        self.assertEqual(self.new_user.last_name, "Macharia")
-        self.assertEqual(self.new_user.phone_number, "0712036591")
-        self.assertEqual(self.new_user.email, "lauramacharia@gmail.com")
-        self.assertEqual(self.new_user.user_name, "LauraMacharia")
-        self.assertEqual(self.new_user.pasword, "ruvy")
-    
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"Laura")
+        self.assertEqual(self.new_user.last_name,"Macharia")
+        self.assertEqual(self.new_user.phone_number,"0712036591")
+        self.assertEqual(self.new_user.email,"lauramacharia@gmail.com")
+        self.assertEqual(self.new_user.username,"LauraMacharia")
+        self.assertEqual(self.new_user.password,"ruvy")
+
+
     def test_save_user(self):
-        """
-        Tests if the user has been saved
-        """
-        
+        '''
+        test_save_user test case to test is the user object is saved into the user
+        '''
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
+        self.assertEqual(len(User.user),1)
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user=[]
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user object to our user
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","0712036591","lauramacharia@gmail.com","LauraMacharia","ruvy")
+        test_user.save_user()
+        self.assertEqual(len(User.user),2)
+
+#more test above
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","0712036591","lauramacharia@gmail.com","LauraMacharia","ruvy")
+        test_user.save_user()
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user), 1)
         
-            
-        
-if _name_ == '_main_':
-    unittest.main()          
+    def test_find_user_by_number(self):
+        '''
+        test to check if we can find a user by phone number and display information
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","0712036591","lauramacharia@gmail.com","LauraMacharia","ruvy")
+        test_user.save_user()
+
+        found_user = User.find_by_number("0712036591")
+        self.assertEqual(found_user.email, test_user.email)
+    def test_user_exist(self):
+        '''
+        test to check if we can return a Boolean if we cannot find the user.
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","0712036591","lauramacharia@gmail.com","LauraMacharia","ruvy")
+        test_user.save_user()
+        user_exists = User.user_exist("LauraMacharia")
+        self.assertTrue(user_exists)
+
+
+    def test_display_all_user(self):
+        '''
+        method that returns a list of all users saved
+        '''
+        self.assertEqual(User.display_user(),User.user)
+
+if __name__ == '__main__':
+        unittest.main()
